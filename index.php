2b8b52b2b0cfa0df3c4bb84a6d69beafbd3b9f88
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-01-17 23:13:29 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Final Space</title>
+        <link rel="shortcut icon" href="./favicon.png"/>
+        <link rel="stylesheet" href="style.css">
+        <script src="main.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <nav class="navbar navbar-inverse">
+                <div class="container-fluid">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="./index.php">Home</a></li>
+                        <li><a href="./characters.php">Personagens</a></li>
+                        <li><a href="./episodes.php">Episódios</a></li>
+                        <li><a href="./location.php">Localizações</a></li>
+                        <li><a href="./quotes.php">Citações</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </body>
+</html>
