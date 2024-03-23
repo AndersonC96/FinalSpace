62244f3dbc25002afc883a33eea00c4e3500fe29
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-03-23 17:15:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,47 @@
 <!DOCTYPE html>
 <html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+bG0E0Kw92eP4O5YNQx0XD8suV5u7KEaP7E1l+E7KMjkvKEzF6TcfpnTae4w5+9G" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
-    <link rel="stylesheet" href="./CSS/style.css">
-    <title>Final Space</title>
-</head>
-<body>
-    <div class="bg-img"></div>
-    <div class="content">
-        <nav class="navbar navbar-expand-lg navbar-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="https://www.netflix.com/pt/title/80174479">Final Space</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="./characters.php">Personagens</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="./episodes.php">Episódios</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="./location.php">Localizações</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="./quotes.php">Citações</a>
-                        </li>
-                    </ul>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css" rel="stylesheet">
+        <link rel="icon" href="./IMG/logo.png" type="image/x-icon" />
+        <title>Final Space</title>
+        <link rel="stylesheet" href="./CSS/style.css">
+    </head>
+    <body>
+        <div class="bg-img"></div>
+        <div class="content">
+            <nav class="navbar navbar-expand-lg navbar-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="https://www.netflix.com/pt/title/80174479">
+                        <img src="./IMG/logo.png" alt="Logo" class="logo-nav">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                    <a class="nav-link" aria-current="page" href="./index.php">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./characters.php">Personagens</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./episodes.php">Episódios</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./location.php">Localizações</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./quotes.php">Citações</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjy8LadH9vhZrNz2+WWUHjI8zNTSZTjCgl3Fd/TvkEnFIF9G7gMdxB" crossorigin="anonymous"></script>
-</body>
+            </nav>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjy8LadH9vhZrNz2+WWUHjI8zNTSZTjCgl3Fd/TvkEnFIF9G7gMdxB" crossorigin="anonymous"></script>
+    </body>
 </html>
